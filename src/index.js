bb0e1011d1ec5b5698427928744bd3bad62a87cf
patirosa95 <patirosa995@gmail.com>
2021-02-11 01:50:36 -0300

    fix: rename route to index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const MainRouter = require('./routes/route');
+const MainRouter = require('./routes');
 
 const app = express();
 
